543f356148a39e6d31c1704283e610aa5c6a9263
F33blecurse <arnediehl96gmail.com>
2016-05-26 23:35:32 +0200

    radius activity

---
+++ app/src/main/java/com/example/bestizer/gpsidea/DistanceActivity.java
@@ -0,0 +1,72 @@
+package com.example.bestizer.gpsidea;
+
+import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import model.AlarmLocation;
+import model.NamedLocation;
+
+public class DistanceActivity extends AppCompatActivity{
+
+    private TextView message;
+    private EditText distanceField;
+    private Button goodnight;
+
+    private NamedLocation namedLocation;
+    private AlarmLocation alarmLocation;
+
+    private double distance;
+
+    /*
+    private double latitude;
+    private double longitude;
+     */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distance);
+
+        message = (TextView)findViewById(R.id.textView);
+        distanceField = (EditText)findViewById(R.id.editText);
+        goodnight = (Button)findViewById(R.id.button);
+
+        Intent i = getIntent();
+        namedLocation = (NamedLocation) i.getParcelableExtra("namedLocation");
+
+        /*
+        Intent intent = getIntent();
+        latitude = intent.getIntExtra("Latit", -1);
+        longitude = intent.getIntExtra("Longit", -1);
+        */
+        
+    }
+
+
+        public void onClick(){
+
+            distance = Integer.parseInt(distanceField.getText().toString());
+            /*
+            Intent intent = new Intent(this,alarm.class);
+            intent.putExtra("Latitude", latitude);
+            intent.putExtra("Longitude", longitude);
+            intent.putExtra("Distance", distance);
+            startActivity(intent);
+            */
+            Intent intent = new Intent(this,alarm.class);
+            Bundle bundle = new Bundle();
+            bundle.putParcelable("alarm", alarmLocation);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+    }
+
+}
--- app/src/main/java/model/NamedLocation.java
+++ app/src/main/java/model/NamedLocation.java
@@ -1,11 +1,14 @@
 package model;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
+
 
 /**
  * @author Hendrik Werner
  */
-public class NamedLocation extends Location {
+public class NamedLocation extends Location implements Parcelable{
 
     public final String name;
 
@@ -13,11 +16,38 @@ public class NamedLocation extends Location {
         super("");
         this.name = name;
     }
+
     public NamedLocation(Location location, String name){
         super(location);
         this.name=name;
     }
 
+    /*protected NamedLocation(Parcel in) {
+        super(in);
+        name = in.readString();
+    }
 
+    public static final Creator<NamedLocation> CREATOR = new Creator<NamedLocation>() {
+        @Override
+        public NamedLocation createFromParcel(Parcel in) {
+            return new NamedLocation(in);
+        }
+
+        @Override
+        public NamedLocation[] newArray(int size) {
+            return new NamedLocation[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(name);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+*/
 
 }
