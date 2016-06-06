1d55a4d431491275d5ff0e6f000aaf573a3f1a90
Hendrikto <hendrik.to@gmail.com>
2016-06-06 17:16:48 +0200

    Clean up the maps activity.

--- app/src/main/java/com/example/bestizer/gpsidea/MapsActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/MapsActivity.java
@@ -3,7 +3,6 @@ package com.example.bestizer.gpsidea;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 
 import model.NamedLocation;
 
@@ -26,56 +25,38 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.text.BreakIterator;
-
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    GoogleApiClient mGoogleApiClient;
-    private GoogleMap mMap;
-    Button getPos;
+    GoogleApiClient googleApiClient;
+    private GoogleMap map;
     double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        //put buttons after setcontentview
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
         }
-        getPos = (Button) this.findViewById(R.id.button2);
-        getPos.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(MapsActivity.this, DistanceActivity.class);
-                NamedLocation nl = new NamedLocation("Map location");
-                nl.setLatitude(latitude);
-                nl.setLongitude(longitude);
-                i.putExtra("model.NamedLocation", nl);
-
-                startActivity(i);
-            }
-        });
     }
 
     @Override
     protected void onStart() {
-        mGoogleApiClient.connect();
+        googleApiClient.connect();
         super.onStart();
     }
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
-    public void checkpermission(GoogleMap mzp) {
+    public void checkPermission(GoogleMap mzp) {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mzp.setMyLocationEnabled(true);
         } else {
@@ -86,33 +67,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Intent intent = getIntent();
         int lat = intent.getIntExtra("Latit", -1);
-        int longit = intent.getIntExtra("Longit", -1);
-        mMap = googleMap;
-        checkpermission(mMap);
+        int lon = intent.getIntExtra("Longit", -1);
+        map = googleMap;
+        checkPermission(map);
         // Add a marker in Sydney and move the camera
-        LatLng custom = new LatLng(lat, longit);
-        mMap.addMarker(new MarkerOptions().position(custom).title("Marker in custom city"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(custom));
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        LatLng custom = new LatLng(lat, lon);
+        map.addMarker(new MarkerOptions().position(custom).title("Marker in custom city"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(custom));
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng point) {
-                // TODO Auto-generated method stub
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(point));
+                map.clear();
+                map.addMarker(new MarkerOptions().position(point));
                 latitude = point.latitude;
                 longitude = point.longitude;
             }
@@ -129,6 +99,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    public void handleChooseButton(View v) {
+        NamedLocation nl = new NamedLocation("Map location");
+        nl.setLatitude(latitude);
+        nl.setLongitude(longitude);
+        Intent i = new Intent(MapsActivity.this, DistanceActivity.class);
+        i.putExtra("model.NamedLocation", nl);
+        startActivity(i);
     }
 
 }
