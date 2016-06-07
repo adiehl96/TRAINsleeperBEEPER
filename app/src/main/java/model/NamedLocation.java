d0db8414264840aec8333bc135e665c2559dff76
Hendrikto <hendrik.to@gmail.com>
2016-06-07 16:39:50 +0200

    Clean up.

--- app/src/main/java/model/NamedLocation.java
+++ app/src/main/java/model/NamedLocation.java
@@ -10,29 +10,28 @@ import android.os.Parcelable;
  */
 public class NamedLocation extends Location {
 
+    public static final Parcelable.Creator<NamedLocation> CREATOR = new Parcelable.Creator<NamedLocation>() {
+        @Override
+        public NamedLocation createFromParcel(Parcel in) {
+            return new NamedLocation(in);
+        }
+
+        @Override
+        public NamedLocation[] newArray(int size) {
+            return new NamedLocation[size];
+        }
+    };
+
     public final String name;
+
     public NamedLocation(String name) {
         super("");
         this.name = name;
     }
 
-
-    public NamedLocation(Location location, String name){
-        super(location);
-        this.name=name;
-    }
-
-    public NamedLocation(NamedLocation nl){
-       super(nl);
-        this.name=nl.name;
-    }
-
-
-
-    protected NamedLocation(Parcel in) {
-        super(Location.CREATOR.createFromParcel(in));
-        name = in.readString();
-
+    public NamedLocation(NamedLocation nl) {
+        super(nl);
+        this.name = nl.name;
     }
 
     @Override
@@ -42,23 +41,13 @@ public class NamedLocation extends Location {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        super.writeToParcel(dest,flags);
+        super.writeToParcel(dest, flags);
         dest.writeString(name);
     }
 
-    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<NamedLocation> CREATOR = new Parcelable.Creator<NamedLocation>() {
-        @Override
-        public NamedLocation createFromParcel(Parcel in) {
-            //Location l = Location.CREATOR.createFromParcel(in);
-            //NamedLocation lp = new NamedLocation(l,in.readString());
+    protected NamedLocation(Parcel in) {
+        super(Location.CREATOR.createFromParcel(in));
+        name = in.readString();
+    }
 
-            return new NamedLocation(in);
-        }
-
-        @Override
-        public NamedLocation[] newArray(int size) {
-            return new NamedLocation[size];
-        }
-    };
 }
