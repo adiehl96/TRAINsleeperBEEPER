4a0c7941aefee20b3389f2a8e9a5b6910246a126
Hendrikto <hendrik.to@gmail.com>
2016-06-07 16:56:10 +0200

    Optimize imports.

--- app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.bestizer.gpsidea;
 
 import android.content.Intent;
-import android.location.Location;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import com.google.android.gms.maps.model.LatLng;
-
 
 public class MainActivity extends AppCompatActivity {
 
