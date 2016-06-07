75489f82a87c58729756a9023be380b00621c439
Hendrikto <hendrik.to@gmail.com>
2016-06-08 01:06:53 +0200

    Remove unused fields.

--- app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
@@ -4,19 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageButton map;
-    private ImageButton train;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        train = (ImageButton) this.findViewById(R.id.imageButton);
-        map = (ImageButton) this.findViewById(R.id.imageButton2);
     }
 
     public void handleMapButton(View v) {
