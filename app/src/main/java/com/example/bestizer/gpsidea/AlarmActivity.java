fd1f4d343d4be333cfce95b56cb656b09bd16f01
Hendrikto <hendrik.to@gmail.com>
2016-06-08 16:16:55 +0200

    Rename layout to match conventions.

--- app/src/main/java/com/example/bestizer/gpsidea/AlarmActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/AlarmActivity.java
@@ -23,7 +23,6 @@ import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -46,7 +45,7 @@ public class AlarmActivity extends AppCompatActivity implements GoogleApiClient.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.alarm);
+        setContentView(R.layout.activity_alarm);
         Intent intent = getIntent();
         destination = intent.getParcelableExtra("model.AlarmLocation");
         currentLocation = new Location("current");
