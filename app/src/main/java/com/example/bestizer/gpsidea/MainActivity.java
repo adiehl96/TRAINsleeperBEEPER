a1c026f9b155532c743dfff5a0d3cb188c2bf7bb
Hendrikto <hendrik.to@gmail.com>
2016-06-08 11:21:16 +0200

    Add a full stop.

--- app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        noInternetToast = Toast.makeText(this, "No Internet Connection available", Toast.LENGTH_SHORT);
+        noInternetToast = Toast.makeText(this, "No Internet Connection available.", Toast.LENGTH_SHORT);
     }
 
     private boolean isNetworkAvailable() {
