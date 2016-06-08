efbf5511180f82acfe348042e24b31cd05aaceee
Hendrik Werner <hendrik.to@gmail.com>
2016-06-09 01:48:32 +0200

    Change permission request handling
    * Start the respective activity when request succeeded after trying to start said activity.
    * Show the respective toast otherwise.

--- app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +13,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int PERMISSIONS_REQUEST_LOCATION_ID = 97;
+    public static final int PERMISSIONS_REQUEST_LOCATION_ID_INITIAL = 97;
+    public static final int PERMISSIONS_REQUEST_LOCATION_ID_TRAIN = 96;
+    public static final int PERMISSIONS_REQUEST_LOCATION_ID_MAP = 95;
 
     private Toast noInternetToast;
     private Toast noLocationPermissionToast;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, MapsActivity.class));
         } else {
             noLocationPermissionToast.show();
-            requestFineLocation();
+            requestFineLocation(PERMISSIONS_REQUEST_LOCATION_ID_MAP);
         }
     }
 
@@ -33,9 +34,26 @@ public class MainActivity extends AppCompatActivity {
             if(!hasFineLocationPermission()) {
                 noLocationPermissionToast.show();
                 requestFineLocation();
-                tryMapsActivity();
             } else if (!networkAvailable()) {
                 noInternetToast.show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_LOCATION_ID_TRAIN) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(MainActivity.this, StationChoiceActivity.class));
+            } else {
+                noLocationPermissionToast.show();
+            }
+        } else if (requestCode == PERMISSIONS_REQUEST_LOCATION_ID_MAP) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            } else {
+                noLocationPermissionToast.show();
             }
         }
     }
@@ -46,19 +64,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         noInternetToast = Toast.makeText(this, "No Internet Connection available.", Toast.LENGTH_SHORT);
         noLocationPermissionToast = Toast.makeText(this, "Location permission is required.", Toast.LENGTH_SHORT);
-        requestFineLocation();
+        requestFineLocation(PERMISSIONS_REQUEST_LOCATION_ID_INITIAL);
 }
 
     private boolean hasFineLocationPermission() {
         return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void requestFineLocation() {
+    private void requestFineLocation(int id) {
         if (!hasFineLocationPermission()) {
             ActivityCompat.requestPermissions(
                     this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_LOCATION_ID
+                    id
             );
         }
     }
