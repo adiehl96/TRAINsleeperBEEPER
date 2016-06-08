948732f30a42f825f75954e3175df12ba68e5965
Hendrikto <hendrik.to@gmail.com>
2016-06-08 15:01:28 +0200

    Update code to use the number picker.

--- app/src/main/java/com/example/bestizer/gpsidea/DistanceActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/DistanceActivity.java
@@ -5,34 +5,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
+import model.AlarmLocation;
 import model.NamedLocation;
 
 public class DistanceActivity extends AppCompatActivity {
 
-    private Toast invalidDistanceToast;
-    private EditText distanceField;
+    private static final int MAX_DISTANCE = 100;
+
+    private NumberPicker distancePicker;
     private NamedLocation namedLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
-        distanceField = (EditText) this.findViewById(R.id.editText);
+        distancePicker = (NumberPicker) findViewById(R.id.distancePicker);
+        distancePicker.setMaxValue(MAX_DISTANCE);
         namedLocation = getIntent().getParcelableExtra("model.NamedLocation");
-        invalidDistanceToast = Toast.makeText(this, "You must enter a valid distance.", Toast.LENGTH_SHORT);
     }
 
     public void handleChooseButton(View v) {
-        try {
-            int distance = Integer.parseInt(distanceField.getText().toString()) * 1000;
-            Intent intent = new Intent(DistanceActivity.this, AlarmActivity.class);
-            intent.putExtra("model.AlarmLocation", new model.AlarmLocation(namedLocation, distance));
-            startActivity(intent);
-        } catch (NumberFormatException ex) {
-            invalidDistanceToast.show();
-        }
+        Intent intent = new Intent(DistanceActivity.this, AlarmActivity.class);
+        intent.putExtra("model.AlarmLocation", new AlarmLocation(namedLocation, distancePicker.getValue() * 1000));
+        startActivity(intent);
     }
 
 }
