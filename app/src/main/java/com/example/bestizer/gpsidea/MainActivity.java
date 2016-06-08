bd89bfa4e4bf86d2a9da784e04271ce87bc62772
Hendrik Werner <hendrik.to@gmail.com>
2016-06-09 01:51:09 +0200

    Add missing import.

--- app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
+++ app/src/main/java/com/example/bestizer/gpsidea/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
